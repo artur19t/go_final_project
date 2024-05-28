7e263bdbd641e36a636d1e75613460dab0e36527
artur19t <artur19t@yandex.ru>
2024-05-29 02:44:40 +0300

    first check

---
+++ db.go
@@ -0,0 +1,101 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"os"
+	"path/filepath"
+	"strconv"
+	"strings"
+	"time"
+
+	_ "modernc.org/sqlite"
+)
+
+func CheckBase() {
+	//appPath := "base"
+
+	dbFile := filepath.Join("base/scheduler.db")
+	_, err := os.Stat(dbFile)
+
+	var install bool
+	if err != nil {
+		fmt.Println(err)
+		install = true
+	}
+
+	if install {
+		fmt.Println("Создание новой базы")
+		_, err = os.Create("base/scheduler.db")
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		db, err := sql.Open("sqlite", "base/scheduler.db")
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		_, err = db.Exec(`
+			CREATE TABLE scheduler (
+				id INTEGER PRIMARY KEY AUTOINCREMENT,
+				date CHAR(8) NOT NULL DEFAULT "",
+				title VARCHAR(256) NOT NULL DEFAULT "",
+				comment TEXT NOT NULL DEFAULT "",
+				repeat VARCHAR(128) NOT NULL DEFAULT ""
+			);
+			CREATE INDEX task_date ON scheduler (date);
+		`)
+		if err != nil {
+			fmt.Println(err)
+		}
+		db.Close()
+	}
+
+}
+
+func NextDate(now time.Time, date string, repeat string) (string, error) {
+	if repeat == "" {
+		return "", nil
+	}
+	slice := strings.Fields(repeat)
+	dateT, err := time.Parse("20060102", date)
+	if err != nil {
+		return "", err
+	}
+	if slice[0] == "d" {
+		if len(slice) > 1 {
+			num, erro := strconv.Atoi(slice[1])
+			if erro != nil {
+				return "-1", erro
+			}
+			if num > 400 {
+				return "-1", nil
+			}
+			timeToReturn := dateT.AddDate(0, 0, num)
+			g1 := timeToReturn.Before(now)
+			for g1 {
+
+				timeToReturn = timeToReturn.AddDate(0, 0, num)
+				g1 = now.After(timeToReturn)
+			}
+
+			strTime := timeToReturn.Format("20060102")
+			return strTime, nil
+		}
+	}
+	if slice[0] == "y" {
+		timeToReturn := dateT.AddDate(1, 0, 0)
+		g1 := timeToReturn.Before(now)
+		for g1 {
+
+			timeToReturn = timeToReturn.AddDate(1, 0, 0)
+			g1 = now.After(timeToReturn)
+		}
+
+		strTime := timeToReturn.Format("20060102")
+		return strTime, nil
+	}
+	return "-1", nil
+}
---
+++ main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+
+	_ "modernc.org/sqlite"
+)
+
+func main() {
+	CheckBase()
+	dir := "web"
+	http.Handle("/api/nextdate", http.HandlerFunc(nextDate))
+	http.Handle("/", http.FileServer(http.Dir(dir)))
+	http.Handle("/api/task", http.HandlerFunc(determinant))
+	http.Handle("/api/tasks", http.HandlerFunc(getTask))
+	http.Handle("/api/task/done", http.HandlerFunc(doneTask))
+	err := http.ListenAndServe(":7540", nil)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func determinant(res http.ResponseWriter, req *http.Request) {
+	method := fmt.Sprint(req.Method)
+	if method == "POST" {
+		postTask(res, req)
+	}
+	if method == "GET" {
+		fillTask(res, req)
+	}
+	if method == "PUT" {
+		correctTask(res, req)
+	}
+	if method == "DELETE" {
+		deleteTask(res, req)
+	}
+}
+
+func nextDate(res http.ResponseWriter, req *http.Request) {
+	now := req.URL.Query().Get("now")
+	date := req.URL.Query().Get("date")
+	repeat := req.URL.Query().Get("repeat")
+	nowT, err := time.Parse("20060102", now)
+	if err != nil {
+		res.Write([]byte(fmt.Sprintf("%T", err)))
+	}
+	dateToRet, _ := NextDate(nowT, date, repeat)
+	res.Write([]byte(dateToRet))
+}
